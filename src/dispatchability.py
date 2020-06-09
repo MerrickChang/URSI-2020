0aef7b419d210847844ef0ef2728dd53a8f701b7
MerrickChang <48596768+MerrickChang@users.noreply.github.com>
2020-06-09 18:45:26 -0400

    6-9-2020 6:45
    Greedy Dispatcher complete (I think?)

--- src/dispatchability.py
+++ src/dispatchability.py
@@ -5,24 +5,23 @@ class Dispatchability:
 
     @staticmethod
     def _get_precessor_edges(stn, node_index):
-        for edge_list in stn.successor_edges[:node_index]:
-            for v, delta in edge_list:
+        for u, edge_list in enumerate(stn.successor_edges[:node_index]):
+            for v, delta in edge_list.items():
                 if v == node_index:
-                    yield (v,delta)
-        for edge_list in stn.successor_edges[node_index+1:]:
-            for v, delta in edge_list:
+                    yield (u, delta)
+        for u, edge_list in enumerate(stn.successor_edges[node_index+1:], start=node_index+1):
+            for v, delta in edge_list.items():
                 if v == node_index:
-                    yield (v,delta)
+                    yield (u, delta)
 
 
 
     @staticmethod
-    def _find_point_in_time_window(A, bounds):
+    def _find_point_in_time_window(A, bounds, time):
         for i, time_point in enumerate(A):
             lower_bound, upper_bound = bounds[time_point]
             if lower_bound <= time and time <= upper_bound:
                 yield A.pop(i)
-
 
     
     @staticmethod
@@ -31,10 +30,10 @@ class Dispatchability:
         n_inf = float("-inf")
         start_index = start
         time = 0
-        length = len(stn.name_dict)
+        length = len(stn.names_dict)
         n = range(length)
         if type(start) == str:
-            start_index = stn.name_dict[start]
+            start_index = stn.names_dict[start]
         A = [start_index]
         A_min = 0
         A_max = 0
@@ -42,27 +41,38 @@ class Dispatchability:
         bounds = []
         execution_time = []
         for x in n:
-            bounds.append((n_inf, p_inf))
+            bounds.append([n_inf, p_inf])
             execution_time.append(p_inf)
-        bounds[start_index] = (0,0)
-        while S < length:
-            for time_point in Dispatchability._find_point_in_time_window(A, bounds):
-                S.append(time_point)
-                execution_time[time_point] = time
-                for v,delta in stn.successor_edges[time_point]:
-                    alt = time+delta
-                    if alt <= bounds[1]:
-                        bounds[1] = alt
-                for _,v,delta in Dispatchability._get_precessor_edges(stn, time_point):
-                    alt = delta-time
-                    if bounds[v][0] <= alt:
-                        bounds[v][0] = alt
-                    if delta<0 and (not (v in A or v in S)):
-                        A.append(v)
+        bounds[start_index] = [0,0]
+        print(stn.names_dict)
+        while len(S) < length:
+            assert len(A) != 0, "There are no more enabled points. This STN is not dispatchable."
             A_min = min([bounds[l][0] for l in A])
-            A_max = max([bounds[u][1] for u in A])
-            if t<A_min:
-                t = A_min
-            assert t<A_max
+            A_max = min([bounds[u][1] for u in A])
+            if time<A_min:
+                time = A_min
+            print("Time =", time)
+            assert time<=A_max, "The time exceeds the maximum value in the enabled points. This STN is not dispatchable."
+            for time_point in Dispatchability._find_point_in_time_window(A, bounds, time):
+                if not time_point in S:
+                    S.append(time_point)
+                execution_time[time_point] = time
+                for v,delta in stn.successor_edges[time_point].items():
+                    alt = time+delta
+                    if bounds[v][1] >= alt:
+                        bounds[v][1] = alt
+                for u,delta in Dispatchability._get_precessor_edges(stn, time_point):
+                    alt = time-delta
+                    if alt >= bounds[u][0]:
+                        bounds[u][0] = alt
+            for u, edge_dict in enumerate(stn.successor_edges):
+                if not (u in A or u in S):
+                    neg_edges_lead_to_S = True
+                    for v, delta in stn.successor_edges[u].items():
+                        if delta<0 and v not in S:
+                            neg_edges_lead_to_S = False
+                            break
+                    if neg_edges_lead_to_S: 
+                        A.append(u)
         for point in S:
-            print(point," at time ", execution_time[t],end=",")
+            print(stn.names_list[point]," at time ", execution_time[point],end=",")
--- src/test.py
+++ src/test.py
@@ -4,14 +4,10 @@ from bellman_ford import BellmanFord
 from dispatchability  import Dispatchability
 from johnson import Johnson
 from floyd_warshall import FloydWarshall
-for test in ["../sample.stn", "../sample2.stn", "../sample3.stn"]:
+for test in ["../sample_dispatchable.stn"]:
     reader = FileReader()
     network =reader.read_file(test)
-    print(Johnson.merrick_johnson(network))
-    print(FloydWarshall.merrick_floyd_warshall(network))
-    print(BellmanFord.merrick_bellman_ford(network, list(network.names_dict.keys())[0]))
-    print(BellmanFord.bannister_eppstein(network, list(network.names_dict.keys())[0]))
     try:
-        Dispatchability.greedy_execute(list(network.names_dict.keys())[0])
-    except:
-        print("Error")
+        Dispatchability.greedy_execute(network, "Z")
+    except AssertionError as e:
+        print(e)
